bb9ef2e7d4b150d46d5c1691fd1eac46e3141a44
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-06-11 10:28:34 -0400

    menu bar and fragments

--- app/src/main/java/com/example/kim_j_project6/MainPageActivity.java
+++ app/src/main/java/com/example/kim_j_project6/MainPageActivity.java
@@ -1,5 +1,6 @@
 package com.example.kim_j_project6;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainPageActivity extends AppCompatActivity {
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Intent myIntent = getIntent();
+        user = myIntent.getParcelableExtra("user");
+
     }
 }
