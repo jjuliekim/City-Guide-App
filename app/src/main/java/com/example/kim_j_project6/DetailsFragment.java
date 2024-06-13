9c505020a64868b558e93996bd03881f0f4625f2
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-06-12 20:05:28 -0400

    place details xml

---
+++ app/src/main/java/com/example/kim_j_project6/DetailsFragment.java
@@ -0,0 +1,37 @@
+package com.example.kim_j_project6;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class DetailsFragment extends Fragment {
+    private Place place;
+    private DatabaseReference placesDatabase;
+
+    public DetailsFragment() {
+    }
+
+    public static DetailsFragment newInstance() {
+        return new DetailsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+        placesDatabase = FirebaseDatabase.getInstance().getReference("places");
+
+        return view;
+    }
+}
